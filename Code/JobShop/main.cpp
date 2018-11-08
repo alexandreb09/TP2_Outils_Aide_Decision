0f9c3ad07235b519a6829e6f166710188e7282b6
GhitaLa <44605286+GhitaLa@users.noreply.github.com>
2018-11-08 09:54:26 +0000

    Update main.cpp

--- Code/JobShop/main.cpp
+++ Code/JobShop/main.cpp
@@ -41,15 +41,16 @@ int main(int, char **) {
 	//rechercheLocal(probleme, sol, n_ite);
 	t_population elite;
 
-	genererPopulationAlea( population,  probleme,  sol, n_ite);
+	genererPopulationAlea( population,  probleme,  sol, n_ite, 0 , 9999);
 	t_solution enfant;
 	//cout << population.liste[1].Bierwirth[3];;
 	croisement(probleme, population.liste[1], population.liste[2], enfant);
-	algoGenetique( probleme,  sol, population,  elite, 30, n_ite, population.nbIndividu - 2);
+	//algoGenetique( probleme,  sol, population,  elite, 30, n_ite, population.nbIndividu - 2);
 	string stop;
 	//cout << endl << endl << endl << "Apres " << n_ite << " iterations, on trouve un makespan : " << sol.makespan << endl << "Appuyer pour sortir (^_-)";
 	cin >> stop;
 
 	return 0;
 }
+
 
