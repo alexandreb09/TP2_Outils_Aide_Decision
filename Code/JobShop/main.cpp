833d0ef75d96f291dfaa713b40d1eaac4e037bf4
GhitaLa <44605286+GhitaLa@users.noreply.github.com>
2018-11-09 09:59:05 +0100

    Update main.cpp

--- Code/JobShop/main.cpp
+++ Code/JobShop/main.cpp
@@ -22,32 +22,21 @@ int main(int, char **) {
 	// afficher_Pinv(probleme);
 	inverser_machine(probleme);
 	// afficher_machine_inv(probleme);
-
-	/* 
-	// Exemple cours 3 x 3
-	sol.Bierwirth[1] = 1;
-	sol.Bierwirth[2] = 2;
-	sol.Bierwirth[3] = 2;
-	sol.Bierwirth[4] = 3;
-	sol.Bierwirth[5] = 3;
-	sol.Bierwirth[6] = 1;
-	sol.Bierwirth[7] = 2;
-	sol.Bierwirth[8] = 1;
-	sol.Bierwirth[9] = 3;
-	*/
-
 	//evaluer(probleme, sol);
 
 	//rechercheLocal(probleme, sol, n_ite);
-	t_population elite;
 
-	genererPopulationAlea( population,  probleme,  sol, n_ite, 0 , 9999);
+	t_population elite;
+	//hashage(elite, sol);
+
+	//genererPopulationAlea( population,  probleme,  sol, n_ite);
+	//selectionBestIndividus(population, 5, elite);
 	t_solution enfant;
 	//cout << population.liste[1].Bierwirth[3];;
-	croisement(probleme, population.liste[1], population.liste[2], enfant);
-	//algoGenetique( probleme,  sol, population,  elite, 30, n_ite, population.nbIndividu - 2);
+	//croisement(probleme, population.liste[1], population.liste[2], enfant);
+	algoGenetique( probleme,  sol, population,  elite, 30, n_ite, population.nbIndividu - 2);
 	string stop;
-	//cout << endl << endl << endl << "Apres " << n_ite << " iterations, on trouve un makespan : " << sol.makespan << endl << "Appuyer pour sortir (^_-)";
+	cout << endl << endl << endl << "Apres " << n_ite << " iterations, on trouve un makespan : " << sol.makespan << endl << "Appuyer pour sortir (^_-)";
 	cin >> stop;
 
 	return 0;
